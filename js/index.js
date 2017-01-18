ff435cc70ecf9bd4e64d06a44333fc0162cc9341
christinasz <christinaszhang@gmail.com>
2017-01-18 14:54:46 -0500

    initial commit
    woohoo

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    /*$("#phoneCarousel").owlCarousel({
+        autoPlay: 6000,
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true,
+    });*/
+    $("#thePortfolio").mixItUp();
+});
