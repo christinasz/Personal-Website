86ed61194c15f40b561dfd2c1005f7583b71aad5
christinasz <christinaszhang@gmail.com>
2017-01-22 18:41:01 -0500

    Finished Portfolio
    - Added link to GitHub
    - Linked resume
    - Smooth scrolling

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
-    /*$("#phoneCarousel").owlCarousel({
-        autoPlay: 6000,
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true,
-    });*/
     $("#thePortfolio").mixItUp();
+});
+
+$(function () {
+    $('a[href*="#"]:not([href="#"],[data-lity])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
